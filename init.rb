b3b0e6eb8501b8460fefde2295fa143f898d7a1b
guillaumequillet <bgachats@yahoo.fr>
2021-10-26 19:52:33 +0200

    dirty replace

--- init.rb
+++ init.rb
@@ -1,153 +1,512 @@
-=begin
-  Gosu Game Jam October 2021
-  Theme : Chaos
-
-  camion arrive : colis tombent en vrac
-  on a une chaine
-  on a des palettes 
-  
-  gameplay :
-  activer / désactiver chaine
-  poser colis sur chaine
-  lancer colis vers opérateur
-  activer opérateur
-
-=end
+# sounds :
+# Beetlemuse, JonnyRuss01, zivs, unadamlar, Mudkip2016 and Kastenfrosch from https://freesound.org/
+# music opengamearts syncopika
 
 require 'gosu'
 
-$tile_size = 16
+$sounds = {
+  'pallet_ok': Gosu::Sample.new('./sfx/423930__mudkip2016__correct.wav'),
+  'box_ok': Gosu::Sample.new('./sfx/476178__unadamlar__correct-choice.wav'),
+  'box_nok': Gosu::Sample.new('./sfx/478191__jonnyruss01__beep-error-1.ogg'),
+  'box_missed': Gosu::Sample.new('./sfx/521973__kastenfrosch__error.ogg'),
+  'box_placed': Gosu::Sample.new('./sfx/433770__zivs__ammo-pickup-1.ogg'),
+  'alert': Gosu::Sample.new('./sfx/529628__beetlemuse__alert-2.wav')
+}
 
-class Position
-  attr_accessor :x, :y, :z
-  def initialize(x = 0, y = 0, z = 0)
-    @x, @y, @z = x, y, z
+$main_score = 0
+$missed_score = -50
+
+class Pallet
+  FONT = Gosu::Font.new(14)
+  COLORS = {
+    green: Gosu::Color.new(255, 76, 255, 0),
+    blue: Gosu::Color.new(255, 72, 0, 255),
+    yellow: Gosu::Color.new(255, 255, 216, 0)
+  }      
+  SCORES = {
+    correct: 20,
+    wrong: 5,
+    slice: 200
+  }
+
+  def initialize(x, y, w, h)
+    @x, @y, @w, @h = x, y, w, h
+    @boxes = []
+    @main_color = nil
+    @correct, @wrong, @score = 0, 0, 0
   end
-end
 
-class Parcel
-  @@gfx = Gosu::Image.new('./gfx/parcel.png', retro: true)
-  def initialize(color, position)
-    @color = color
-    @position = position
+  def get_next_destination
+    # will be slices of 2 x 5 boxes
+    box_count = @boxes.size + 1
+
+    offset_x = 0
+    offset_y = 0
+
+    case box_count
+    when 1
+      offset_x = 0
+      offset_y = 0
+    when 2
+      offset_x = 0
+      offset_y = 1
+    when 3
+      offset_x = 0
+      offset_y = 2
+    when 4
+      offset_x = 0
+      offset_y = 3
+    when 5
+      offset_x = 0
+      offset_y = 4
+    when 6
+      offset_x = 1
+      offset_y = 0
+    when 7
+      offset_x = 1
+      offset_y = 1
+    when 8
+      offset_x = 1
+      offset_y = 2
+    when 9
+      offset_x = 1
+      offset_y = 3
+    when 10
+      offset_x = 1
+      offset_y = 4
+    end
+
+    x = @x + 4 + offset_x * 48
+    y = @y + 4 + offset_y * 33
+
+    {x: x, y: y}
   end
 
-  def draw(position = @position)
-    @@gfx.draw(position.x, position.y, position.z, 1, 1, @color)
+  def add_box(box)
+    destination = get_next_destination
+    box.set_destination(destination[:x], destination[:y])
+    @boxes.push(box)
+
+    if @main_color.nil?
+      @main_color = box.color
+      @correct += 1
+      @score += SCORES[:correct]
+      $sounds[:box_ok].play
+    else
+      if @main_color != box.color
+        @wrong += 1
+        @score += SCORES[:wrong]
+        $sounds[:box_nok].play
+      else
+        @correct += 1
+        @score += SCORES[:correct]
+        $sounds[:box_ok].play
+      end
+    end
+
+    if @boxes.size == 10
+      # if all 10 are placed, we'll add extra score if color is unique
+      colors = []
+      @boxes.each {|box| colors.push box.color}
+      if colors.uniq.size == 1
+        @score += SCORES[:slice]
+        $sounds[:pallet_ok].play
+      end
+
+      # we reset the pallet
+      $main_score += @score
+      @boxes = []
+      @correct = 0
+      @wrong = 0
+      @score = 0
+      @main_color = nil
+    end
+  end
+
+  def update
+    @boxes.each {|box| box.update}
+  end
+
+  def draw
+    @boxes.each {|box| box.draw}
+
+    unless @main_color.nil?
+      color = case @main_color
+      when 'GREEN' then COLORS[:green]
+      when 'BLUE' then COLORS[:blue]
+      when 'YELLOW' then COLORS[:yellow]
+      end
+
+      Gosu.draw_rect(@x + 61, 25, 30, 14, color)
+    end
+    FONT.draw_text(@correct, @x + 76, 45, 1, 1, 1, Gosu::Color::GREEN)
+    FONT.draw_text(@wrong, @x + 61, 62, 1, 1, 1, Gosu::Color::RED)
+    FONT.draw_text(@score, @x + 58, 89, 1, 1, 1, Gosu::Color::BLACK)
   end
 end
 
 class Conveyor
-  @@gfx = Gosu::Image.load_tiles('./gfx/conveyor.png', 16, 32, retro: true)
-  @@control_panel = {
-    speed_1: Gosu::Image.new('./gfx/conveyor_control_panel/speed_1.png'),
-    speed_2: Gosu::Image.new('./gfx/conveyor_control_panel/speed_2.png'),
-    speed_3: Gosu::Image.new('./gfx/conveyor_control_panel/speed_3.png'),
-    speed_stop: Gosu::Image.new('./gfx/conveyor_control_panel/stop.png')
-  }
+  SPEEDS = [0, 0.5, 1, 4]
+  CONTROL_PANEL = [
+    Gosu::Image.new('./gfx/conveyor_control_panel/stop.png', retro: true),
+    Gosu::Image.new('./gfx/conveyor_control_panel/speed_1.png', retro: true),
+    Gosu::Image.new('./gfx/conveyor_control_panel/speed_2.png', retro: true),
+    Gosu::Image.new('./gfx/conveyor_control_panel/speed_3.png', retro: true)
+  ]
 
-  def initialize(window)
+  def initialize(window, x, y, w, h)
     @window = window
-    set_speed(1)
-    @current_frame = 0
-    @frame_tick = Gosu::milliseconds
-    @pieces_tiles = []
-
-    @ui_position = Position.new(0, @window.height - @@control_panel[:speed_1].height, 1)
+    @x, @y, @w, @h = x, y, w, h
+    @ejection_spots = []
+    @boxes = []
+    @stripe = Gosu::Image.new('./gfx/conveyor_stripe.png', retro: true)
+    @offset_stripe = 0
+    @speed = SPEEDS.first
   end
 
-  def set_speed(speed)
-    @speed = speed
-    @frame_duration = case speed
-    when 0 then 0
-    when 1 then 400
-    when 2 then 200
-    when 3 then 50
+  def set_entry_spot(x, y)
+    @entry_spot_x = x
+    @entry_spot_y = y
+  end
+
+  def add_ejection_spot(x, y, w, h, pallet)
+    spot = {x: x, y: y, w: w, h: h, pallet: pallet}
+    @ejection_spots.push spot
+  end
+  
+  def add_box(box)
+    # if no box overlaps the entry spot
+    if @entry_spot_available
+      box.set_destination(@entry_spot_x, @entry_spot_y)
+      @boxes.push box
+      $sounds[:box_placed].play
+      return true
+    else
+      return false
     end
+  end
+
+  def remove_box(box)
+    @boxes.delete(box)
   end
 
   def button_down(id)
     if id == Gosu::MS_LEFT
-      if @window.mouse_in?(@ui_position.x + 58, @ui_position.y + 123, 36, 36)
-        set_speed(1)
-      elsif @window.mouse_in?(@ui_position.x + 101, @ui_position.y + 123, 36, 36)
-        set_speed(2)
-      elsif @window.mouse_in?(@ui_position.x + 144, @ui_position.y + 123, 36, 36)
-        set_speed(3)
-      elsif @window.mouse_in?(@ui_position.x + 188, @ui_position.y + 123, 128, 36)
-        set_speed(0)
+      handle_mouse
+    end
+    @speed = SPEEDS.first if id == Gosu::KB_TAB
+    @speed = SPEEDS[1] if id == Gosu::KB_1
+    @speed = SPEEDS[2] if id == Gosu::KB_2
+    @speed = SPEEDS[3] if id == Gosu::KB_3
+    eject_box(@ejection_spots[0]) if id == Gosu::KB_Q
+    eject_box(@ejection_spots[1]) if id == Gosu::KB_W
+    eject_box(@ejection_spots[2]) if id == Gosu::KB_E
+  end
+
+  def handle_mouse
+    @speed = SPEEDS.first if @window.mouse_in?(188, 526, 128, 36)
+    @speed = SPEEDS[1] if @window.mouse_in?(58, 526, 36, 36)
+    @speed = SPEEDS[2] if @window.mouse_in?(101, 526, 36, 36)
+    @speed = SPEEDS[3] if @window.mouse_in?(144, 526, 36, 36)
+
+    @ejection_spots.each do |es|
+      if @window.mouse_in?(es[:x], es[:y], es[:w], es[:h])
+        eject_box(es)
       end
     end
   end
 
-  def add_piece(tile_position)
-    @pieces_tiles.push tile_position
+  def eject_box(es)
+    candidates = []
+    @boxes.each do |box|
+      candidates.push box if box.x + Box::WIDTH >= es[:x] && box.x <= es[:x] + es[:w]
+    end
+
+    unless candidates.empty?
+      # we push the furthest box to the pallet area
+      candidate = candidates.sort {|a, b| a.x <=> b.x}.first
+      es[:pallet].add_box(candidate)
+      remove_box(candidate)
+    end
   end
 
   def update
-    if @speed > 0 
-      if Gosu::milliseconds - @frame_tick >= @frame_duration
-        @current_frame += 1
-        @current_frame = 0 if @current_frame >= @@gfx.size
-        @frame_tick = Gosu::milliseconds
+    to_delete = []
+    @entry_spot_available = @boxes.select {|box| box.x + Box::WIDTH >= @entry_spot_x}.empty?
+
+    @boxes.each do |box| 
+      if box.state == :on_conveyor
+        dest_x = box.x - @speed
+        box.set_destination(dest_x, box.y)
       end
-    else
-      @frame_tick = Gosu::milliseconds
+      box.update
+
+      # check for out of bounds box
+      if box.x + Box::WIDTH <= 0
+        $sounds[:box_missed].play
+        to_delete.push box
+        $main_score += $missed_score
+      end
     end
+
+    to_delete.each {|box| @boxes.delete(box)}
+
+    @ejection_spots.each do |es|
+      es[:pallet].update
+    end
+
+    @offset_stripe -= @speed
+    @offset_stripe = 0 if @offset_stripe <= -80.0
   end
 
   def draw
-    @pieces_tiles.each do |coordinates|
-      x = coordinates.x * $tile_size
-      y = coordinates.y * $tile_size
-      @@gfx[@current_frame].draw(x, y, 0)
-    end
-  end
+    # Gosu.draw_rect(@x, @y, @w, @h, Gosu::Color.new(255, 255, 0, 255))
+    entry_spot_color = @entry_spot_available ? Gosu::Color::GREEN : Gosu::Color::RED
+    Gosu.draw_rect(@entry_spot_x, @entry_spot_y + 1, 48, 47, entry_spot_color)
+    @boxes.each {|box| box.draw}
 
-  def draw_ui
-    # control panel drawing
-    image = case @speed
-    when 0 then :speed_stop
-    when 1 then :speed_1
-    when 2 then :speed_2
-    when 3 then :speed_3
+    # ejection spots
+    @ejection_spots.each do |es| 
+      color = Gosu::Color.new(255, 128, 128, 128)
+      @boxes.each do |box|
+        if box.x + Box::WIDTH >= es[:x] && box.x <= es[:x] + es[:w]
+          color = Gosu::Color.new(255, 180, 180, 180)
+          break
+        end
+      end
+      Gosu.draw_rect(es[:x], es[:y] + 1, es[:w], es[:h] - 1, color)
     end
-    @@control_panel[image].draw(@ui_position.x, @ui_position.y, @ui_position.z)
+
+    @ejection_spots.each do |es|
+      es[:pallet].draw
+    end
+
+    # stripes
+    10.times do |x|
+      @stripe.draw(x * 80 + @offset_stripe, 337, 2)
+    end
+
+    # control panel drawing
+    CONTROL_PANEL[SPEEDS.index(@speed)].draw(0, 600 - 196, 1)
   end
 end
 
-class Pallet
-  @@gfx = Gosu::Image.new('./gfx/pallet.png', retro: true)
-  def initialize(position)
-    @position = position
-    @parcels = []
-    @max_parcels = 30
+class SpawnArea
+  def initialize(x, y, w, h)
+    @x, @y, @w, @h = x, y, w, h
+    @boxes = []
+    @boxes_max = 20
+    @font = Gosu::Font.new(24)
   end
 
-  def add_parcel(parcel)
-    @parcels.push parcel if @parcels.size < @max_parcels
+  def includes_point?(x, y)
+    (x >= @x && x <= @x + @w && y >= @y && y <= @y + @h)
+  end
+
+  def pick_box(x, y)
+    candidates = []
+    @boxes.each do |box|
+      if box.includes_point?(x, y)
+        candidates.push box
+      end
+    end
+
+    return candidates.empty? ? nil : candidates.sort {|a, b| a.y <=> b.y}.last
+  end
+
+  def spawn_box
+    return false if @boxes.size + 1 >= @boxes_max
+    x = Gosu.random(@x, @x + @w - Box::WIDTH).floor
+    y = Gosu.random(@y, @y + @h - Box::HEIGHT).floor
+    spawn_x = x
+    spawn_y = 10
+    box = Box.new(spawn_x, spawn_y)
+    box.set_destination(x, y)
+    @boxes.push(box)
+    $sounds[:alert].play(0.25, 0.75) if @boxes.size >= 18
+    return true
+  end
+
+  def remove_box(box)
+    @boxes.delete(box)
+  end
+
+  def update
+    @boxes.each {|box| box.update}
   end
 
   def draw
-    @@gfx.draw(@position.x * $tile_size, @position.y * $tile_size, @position.z)
-    slice = 0
-    height = 0
-    x, y = 0, -3
-    @parcels.each_with_index do |parcel, i|
-      parcel.draw(Position.new(@position.x * $tile_size + x, @position.y * $tile_size + y, 0))
-      y += 5
-      slice += 1
-      
-      # 5 boxes side by side
-      if slice == 5
-        x += 8
-        y = -3 - height
-      # we can place up to 10 boxes on one slice
-      elsif slice >= 10
-        slice = 0
-        height += 4
-        x, y = 0, -3 - height
+    # Gosu.draw_rect(@x, @y, @w, @h, Gosu::Color::BLUE)
+    @boxes.each {|box| box.draw}
+    color = @boxes.size >= 18 ? Gosu::Color::RED : Gosu::Color::BLACK
+    @font.draw_text("Boxes : #{@boxes.size}/#{@boxes_max}", 660, 45, 2, 1, 1, color)
+  end
+end
+
+class Box
+  attr_accessor :x, :y, :state, :color
+  WIDTH = 48
+  HEIGHT = 32
+  @@gfx = Gosu::Image.load_tiles('./gfx/box.png', WIDTH, HEIGHT, retro: true)
+  VELOCITY = 10.0
+
+  def initialize(x, y)
+    @x, @y = x, y
+    set_destination(@x, @y)
+    @box_image = @@gfx.sample
+    @color = ['GREEN', 'BLUE', 'YELLOW'][@@gfx.index(@box_image)]
+    @states = [:on_truck, :on_spawn, :on_conveyor, :on_pallet]
+    @state = :on_truck
+  end
+  
+  def set_destination(x, y)
+    @dest_x, @dest_y = x, y
+  end
+
+  def includes_point?(x, y)
+    (x >= @x && x <= @x + @box_image.width && y >= @y && y <= @y + @box_image.height)
+  end
+
+  def update
+    if @x != @dest_x || @y != @dest_y
+      if @x > @dest_x
+        @x -= VELOCITY
+        @x = @dest_x if @x < @dest_x
       end
+      if @x < @dest_x
+        @x += VELOCITY
+        @x = @dest_x if @x > @dest_x
+      end
+      if @y > @dest_y
+        @y -= VELOCITY
+        @y = @dest_y if @y < @dest_y
+      end
+      if @y < @dest_y
+        @y += VELOCITY
+        @y = @dest_y if @y > @dest_y
+      end
+
+      # we reached the next position
+      if @x == @dest_x && @y == @dest_y
+        if @state == :on_truck
+          @state = :on_spawn
+        elsif @state == :on_spawn
+          @state = :on_conveyor
+        end
+      end
+    end
+  end
+
+  def draw
+    @box_image.draw(@x, @y, @y)
+  end
+end
+
+class Game
+  def initialize(window)
+    @state = :title
+    @window = window
+    @gfx = {
+      title: Gosu::Image.new('./gfx/title.png', retro: true),
+      background: Gosu::Image.new('./gfx/background.png', retro: true),
+      tutorial: Gosu::Image.new('./gfx/tutorial.png', retro: true)
+    }
+
+    setup_game
+
+    @font = Gosu::Font.new(48)
+    @music = Gosu::Song.new('./sfx/funbgm102614.ogg')
+  end
+
+  def setup_game
+    $main_score = 0
+
+    @conveyor = Conveyor.new(@window, 2, 337, 600, 32)
+    @conveyor.set_entry_spot(752, 337)
+    @conveyor.add_ejection_spot(20, 337, 103, 48, Pallet.new(20, 142, 103, 195))
+    @conveyor.add_ejection_spot(197, 337, 103, 48, Pallet.new(197, 142, 103, 195))
+    @conveyor.add_ejection_spot(374, 337, 103, 48, Pallet.new(374, 142, 103, 195))
+
+    @spawn_area = SpawnArea.new(800 - 160, 128, 160, 200)
+    @boxes_initial_qty = 5
+    @boxes_initial_qty.times {@spawn_area.spawn_box} 
+    @box_tick = Gosu::milliseconds
+    @box_time_before_next = 2000
+  end
+
+  def button_down(id)
+    case @state
+    when :title
+      # whatever key pressed starts the game
+      @state = :tutorial
+    when :tutorial
+      @state = :game
+      @music.play(true)
+    when :game
+      @conveyor.button_down(id)
+      if id == Gosu::MS_LEFT
+        handle_mouse
+      end
+    when :game_over
+      # whatever key pressed restarts the game
+      setup_game
+      @state = :game
+    end
+  end
+
+  def handle_mouse
+    x = @window.mouse_x
+    y = @window.mouse_y
+
+    # box picking
+    if @spawn_area.includes_point?(x, y)
+      box = @spawn_area.pick_box(x, y)
+      unless box.nil?
+        x = Gosu.random(0, 50)
+        y = Gosu.random(0, @window.height - 30)
+        # we move the box from spawn to conveyor if space is available on conveyor
+        added = @conveyor.add_box(box)
+        @spawn_area.remove_box(box) if added
+      end
+    end
+  end
+
+  def auto_spawn
+    if Gosu::milliseconds - @box_tick >= @box_time_before_next
+      spawn = @spawn_area.spawn_box
+      @state = :game_over if spawn == false
+      @box_tick = Gosu::milliseconds
+    end
+  end
+
+  def update
+    case @state
+    when :game
+      auto_spawn
+      @spawn_area.update
+      @conveyor.update
+    end
+  end
+
+  def draw
+    case @state
+    when :title
+      @gfx[:title].draw(0, 0, 0)
+    when :tutorial
+      @gfx[:tutorial].draw(0, 0, 0)
+    when :game
+      @gfx[:background].draw(0, 0, 0)
+      @spawn_area.draw
+      @conveyor.draw
+      @font.draw_text("Score : #{$main_score}", 460, 500, 1, 1, 1, Gosu::Color::BLACK)
+    when :game_over
+      @gfx[:background].draw(0, 0, 0)
+      @spawn_area.draw
+      @conveyor.draw
+      Gosu.draw_rect(0, 0, @window.width, @window.height, Gosu::Color.new(128, 0, 0, 0), 10000)
+      @font.draw_text('Game Over', 50, 50, 10001, 2, 2, Gosu::Color::WHITE)
+      @font.draw_text('Press any key to restart', 50, 140, 10001, 1, 1, Gosu::Color::WHITE)
+      @font.draw_text("Score : #{$main_score}", 50, 230, 10001, 2, 2, Gosu::Color::WHITE)
     end
   end
 end
@@ -155,46 +514,26 @@ end
 class Window < Gosu::Window
   def initialize
     super(800, 600, false)
-    self.caption = 'Gosu Game JAM - October 2021 - Theme : Chaos'
-    map_setup
-  end
-
-  def map_setup
-    @pallet = Pallet.new(Position.new(1, 1))
-    @conveyor = Conveyor.new(self)
-    @conveyor.add_piece(Position.new(2, 4))
-    @conveyor.add_piece(Position.new(3, 4))
-    @conveyor.add_piece(Position.new(4, 4))
-    @conveyor.add_piece(Position.new(5, 4))
-  end
-
-  def mouse_in?(x, y, w, h)
-    self.mouse_x >= x && self.mouse_x <= x + w && self.mouse_y >= y && self.mouse_y <= y + h
+    self.caption = 'Boxes ! - Gosu Game Jam Oct 2021'
+    @game = Game.new(self)
   end
 
   def button_down(id)
     super
     close! if id == Gosu::KB_ESCAPE
-    @conveyor.button_down(id)
+    @game.button_down(id)
+  end
 
-    if id == Gosu::KB_SPACE
-      colors = [Gosu::Color::RED, Gosu::Color::GREEN, Gosu::Color::BLUE]
-      @pallet.add_parcel(Parcel.new(colors.sample, Position.new))
-    end
+  def mouse_in?(x, y, w, h)
+    (self.mouse_x >= x && self.mouse_x <= x + w && self.mouse_y >= y && self.mouse_y <= y + h)
   end
 
   def update
-    @conveyor.update
+    @game.update
   end
 
   def draw
-    zoom = 2
-    scale(zoom, zoom) do
-      @pallet.draw
-      @conveyor.draw
-    end
-
-    @conveyor.draw_ui
+    @game.draw
   end
 end
 
